f63270e611bc43b6b4e5888b9ccd4ad5143d39b9
Chelsea <chelseack.wong@mail.utoronto.ca>
2023-09-11 14:33:22 -0400

    reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,13 +1,16 @@
 public class Reduce {
     public static void main(String[] args) {
+        int i = 100;
         doReduce(i);
     }
 
     public static void doReduce(int i) {
         int j = i;
         int count = 0;
-        if (j != 0){
-            if (i % 2 == 0) {
+        while (true){
+            boolean b = j != 0;
+            if (!b) break;
+            if (j % 2 == 0) {
                 j = j / 2;
                 count ++;
             } else {
